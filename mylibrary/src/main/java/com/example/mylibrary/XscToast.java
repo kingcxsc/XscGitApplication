a13a25c68674f84d2b1c001d89e5ad4267dec54e
徐圣灿 <1280939520@qq.com>
2025-11-29 14:58:28 +0800

    first commit

---
+++ mylibrary/src/main/java/com/example/mylibrary/XscToast.java
@@ -0,0 +1,10 @@
+package com.example.mylibrary;
+
+import android.content.Context;
+import android.widget.Toast;
+
+public class XscToast {
+    public static void toast(Context context){
+        Toast.makeText(context,"XscToast测试",Toast.LENGTH_SHORT).show();
+    }
+}
